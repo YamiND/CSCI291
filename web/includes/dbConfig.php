90d524e714cbdf1c089b65e64fea96b9171de69a
Tyler Postma <tpostma@lssu.edu>
2017-02-16 02:10:35 +0000

    our default theme configuration

---
+++ web/includes/dbConfig.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "bioGrade");    // The database username. 
+define("PASSWORD", "bioGrade");    // The database password. 
+define("DATABASE", "bioRubricDB");    // The database name.
+
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+define("DEBUG", true);
+?>
---
+++ web/index.php
@@ -0,0 +1,15 @@
+<?php include("includes/customizations.php");?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/login">
+<title><?php echo aliasSystem; ?></title>
+<script language="javascript">
+    window.location.href = "pages/login"
+</script>
+</head>
+<body>
+Go to <a href="pages/index">/pages/login</a>
+</body>
+</html>
